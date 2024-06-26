5221751b08947f58c42c5e2bec288ca96e3d5e12
Espen Moe-Nilssen <espen@medialog.no>
2024-06-26 15:50:56 +0200

    add control panel and more basic button

--- src/medialog/whatsappbutton/interfaces.py
+++ src/medialog/whatsappbutton/interfaces.py
@@ -2,7 +2,42 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from medialog.controlpanel.interfaces import IMedialogControlpanelSettingsProvider
+
+
+from zope import schema
+from zope.interface import alsoProvides
+from plone.supermodel import model
+ 
 
 
 class IMedialogWhatsappbuttonLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+    
+
+class ISettings(model.Schema):
+    """Adds settings to medialog.controlpanel
+    """
+
+    model.fieldset(
+        'whatsapp_menu',
+        label=(u'WhatsAppButton'),
+        fields=[
+            'phone', 
+            'color',
+            ],
+        )
+
+     
+    phone =  schema.Int(
+        title="phone", 
+    )
+
+    color =  schema.TextLine(
+        title="Button Color", 
+    )
+
+
+
+alsoProvides(ISettings, IMedialogControlpanelSettingsProvider)
--- src/medialog/whatsappbutton/viewlets/whats_app_viewlet.py
+++ src/medialog/whatsappbutton/viewlets/whats_app_viewlet.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from plone import api
 
 
 class WhatsAppViewlet(ViewletBase):
 
     def update(self):
-        self.message = self.get_message()
+        self.phone = self.get_phone()
+        self.color = self.get_color()
 
-    def get_message(self):
-        return u'My message'
+    def get_phone(self):
+        phone = api.portal.get_registry_record('medialog.whatsappbutton.interfaces.ISettings.phone', default='')
+        return phone
+
+    def get_color(self):
+        color = api.portal.get_registry_record('medialog.whatsappbutton.interfaces.ISettings.color', default='#123456')
+        return color
 
     def index(self):
         return super(WhatsAppViewlet, self).render()
